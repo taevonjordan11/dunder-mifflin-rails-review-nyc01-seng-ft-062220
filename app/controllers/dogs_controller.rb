eeec2d0656aa7666c3e2afdeb4137ad09c0a834e
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-22 12:01:50 -0400

    working on show page for dogs

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,19 @@
 class DogsController < ApplicationController
+
+  def index
+    @dogs = Dog.all
+  end
+
+  def show
+    @dog = Dog.find(params[:id])
+  end
+
+
+
+
+
+  private
+  def dog_params
+    require(:dog).permit(:name, :breed, :age)
+  end
 end
--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,2 +1,13 @@
 class EmployeesController < ApplicationController
+
+
+
+
+
+
+  
+  private
+  def employee_params
+    require(:employee).permit(:first_name, :last_name, :alias, :title, :office, :img_url)
+  end
 end
