97004758d3c6938bfb2dd625d1b6baf298fbdbff
Taevon Jordan <nude.plus@MacBook-Pro.home>
2020-07-22 23:45:16 -0400

    validations for forms

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,3 +1,12 @@
 class Employee < ApplicationRecord
   belongs_to :dog
+
+
+  validates :first_name, uniqueness: true
+
+  validates :last_name, uniqueness: true
+
+  validates :alias, uniqueness: true
+
+  validates :title, uniqueness: true
 end
